ab9a8158c4d98a4985e4b20c484b9b806b61be08
hicbag <thomas.ng@sjsu.edu>
2021-10-09 23:00:03 -0700

    Changed statements to satisfy conditions of test cases

--- calculator.py
+++ calculator.py
@@ -3,21 +3,21 @@ def calculator(number1, number2, operator):
     opers = ['+', '-', '*', '/', '//', '**']
     if operator in opers: 
         if operator==opers[0]:
-            return number1+number2
+            return float(number1+number2)
         if operator==opers[1]:
-            return number1-number2
+            return float(number1-number2)
         if operator==opers[2]:
-            return number1*number2
+            return float(number1*number2)
         if operator==opers[3]:
             if number2==0:
-                return 'Invalid operation'
-            return number1/number2
+                return False
+            return float(number1/number2)
         if operator==opers[4]:
             if number2==0: 
-                return 'Invalid operation'
-            return number1//number2
+                return False
+            return float(number1//number2)
         if operator==opers[5]:
-            return number1**number2
+            return float(number1**number2)
     else: 
         print('Invalid Operation')
 
@@ -25,8 +25,8 @@ def parse_input():
     '''Asks for an input equation, and splits the equation into the 3 tokens for the calculator function'''
     inp = input("Enter equation: ")
     args = inp.split(" ")
-    a=float(args[0])
-    b=float(args[2])
+    a=int(args[0])
+    b=int(args[2])
     op = args[1]
     print(str(calculator(a, b, op)))
 
